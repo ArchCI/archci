5c325edcf11c0c8d8a7c3472d6ba9c6b1dcb7eca
wizawu <wizawu@gmail.com>
2015-06-06 09:57:50 +0800

    Add BaseStore

--- static/js/AppConstants.js
+++ static/js/AppConstants.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const keyMirror = require("react/lib/keyMirror");
 
 export default {
--- static/js/AppDispatcher.js
+++ static/js/AppDispatcher.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import {Dispatcher} from "flux";
 
 export default new Dispatcher();
