2096121b237b65b9f2418b9efc65a0cb5436c006
tobe <tobeg3oogle@gmail.com>
2015-06-06 20:32:58 +0800

    Add basic framework for api service

---
+++ controllers/api.go
@@ -0,0 +1,94 @@
+package controllers
+
+import (
+	"github.com/golang/glog"
+	"github.com/astaxie/beego"
+)
+
+type ApiController struct {
+	beego.Controller
+}
+
+/* Create acount */
+func (c *ApiController) CreateAccount() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetActiveBuilds() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetSearchBuilds() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetBuildLog() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetBuildLogPart() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) CreateProject() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetProjects() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) CreateImage() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetImages() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetImage() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetTasks() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) FinishTask() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+func (c *ApiController) GetWorkers() {
+	glog.Info("Creat accout")
+
+	c.Ctx.WriteString("")
+}
+
+
+
+
+
--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,24 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+
+	beego.Router("/v1/account", &controllers.ApiController{}, "post:CreateAccount")
+	// login
+
+	beego.Router("/v1/builds/active", &controllers.ApiController{}, "get:GetActiveBuilds")
+	beego.Router("/v1/builds/search", &controllers.ApiController{}, "get:GetSearchBuilds")
+	beego.Router("/v1/builds/:id/log", &controllers.ApiController{}, "get:GetBuildLog")
+	beego.Router("/v1/builds/:id/log", &controllers.ApiController{}, "get:GetBuildLogPart")
+
+	beego.Router("/v1/projects", &controllers.ApiController{}, "post:CreateProject")
+	beego.Router("/v1/projects", &controllers.ApiController{}, "get:GetProjects")
+
+	beego.Router("/v1/images", &controllers.ApiController{}, "post:CreateImage")
+	beego.Router("/v1/images", &controllers.ApiController{}, "get:GetImages")
+	beego.Router("/v1/images/:id", &controllers.ApiController{}, "get:GetImage")
+
+	beego.Router("/v1/tasks", &controllers.ApiController{}, "get:GetTasks")
+	beego.Router("/v1/tasks/:id", &controllers.ApiController{}, "put:FinishTask")
+
+	beego.Router("/v1/workers", &controllers.ApiController{}, "get:GetWorkers")
 }
