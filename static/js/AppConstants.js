6f0f133872b86803afaad7e11ef8881c9d42b129
wizawu <wizawu@gmail.com>
2015-06-06 09:53:42 +0800

    Add basic react utils

---
+++ static/js/AppConstants.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const keyMirror = require("react/lib/keyMirror");
+
+export default {
+  ActionTypes: keyMirror({
+    NULL: null
+  })
+};
---
+++ static/js/AppDispatcher.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import {Dispatcher} from "flux";
+
+export default new Dispatcher();
