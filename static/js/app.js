3a9ca4f1eec49f9ac8bd9f343143aa586eb8a8c3
tobe <tobeg3oogle@gmail.com>
2015-07-19 22:38:09 +0800

    Translate to simplify and tranditional Chinese

--- static/js/app.js
+++ static/js/app.js
@@ -60,7 +60,7 @@ archci.controller('IndexController', function ($scope, $rootScope, $translate, $
 
   /* Determine it is simplified Chinese or not */
   $scope.isZhCn = function () {
-           return $translate.use() == "zh-cn";
+     return $translate.use() == "zh-cn";
   }
 
   /* Determine it is traditional Chinese or not */
@@ -68,41 +68,66 @@ archci.controller('IndexController', function ($scope, $rootScope, $translate, $
      return $translate.use() == "zh-hant";
   }
 
-    /* Determine it is German or not */
-    $scope.isDeDe = function () {
-       return $translate.use() == "de-de";
-    }
-
-    /* Determine it is French or not */
-    $scope.isFrFr = function () {
-       return $translate.use() == "fr-fr";
-    }
-
- });
+});
 
 
- /* Use angular-translate for i18n and all text should be translated here */
- archci.config(function ($translateProvider) {
-   /* Use cookie to store the perference of i18n language */
-   $translateProvider.useCookieStorage();
+/* Use angular-translate for i18n and all text should be translated here */
+archci.config(function ($translateProvider) {
+  /* Use cookie to store the perference of i18n language */
+  $translateProvider.useCookieStorage();
 
-   /* The default language should be English */
-   $translateProvider.preferredLanguage('en-us');
+  /* The default language should be English */
+  $translateProvider.preferredLanguage('en-us');
 
-   /* Translate into English */
-   $translateProvider.translations('en-us', {
-     // Index page
-     archci: 'ArchCI',
-     builds: 'Builds',
-     projects: 'Projects',
-     workers: 'Workers',
-     more: 'More',
-     en_us: 'English',
-     zh_cn: '简体中文',
-     zh_hant: '繁體中文',
+  /* Translate into English */
+  $translateProvider.translations('en-us', {
+    // Index page
+    archci: 'ArchCI',
+    builds: 'Builds',
+    projects: 'Projects',
+    workers: 'Workers',
+    more: 'More',
+    en_us: 'English',
+    zh_cn: '简体中文',
+    zh_hant: '繁體中文',
 
-     // Build page
-     no_search_result_for: 'No search result for',
+    // Build page
+    no_search_result_for: 'No search result for',
+    search: 'Search',
+  });
+
+  /* Translate into simplified Chinese */
+  $translateProvider.translations('zh-cn', {
+    // Index page
+    archci: 'ArchCI',
+    builds: '持续集成',
+    projects: '所有项目',
+    workers: '所有节点',
+    more: '更多',
+    en_us: 'English',
+    zh_cn: '简体中文',
+    zh_hant: '繁體中文',
+
+    // Build page
+    no_search_result_for: '找不到相关搜索结果',
+    search: '搜索',
+  });
+
+  /* Translate into traditional Chinese */
+  $translateProvider.translations('zh-hant', {
+    // Index page
+    archci: 'ArchCI',
+    builds: '持續集成',
+    projects: '所有項目',
+    workers: '所有節點',
+    more: '更多',
+    en_us: 'English',
+    zh_cn: '简体中文',
+    zh_hant: '繁體中文',
+
+    // Build page
+    no_search_result_for: '找不到相關搜索結果',
+    search: '搜索',
   });
 
 });
