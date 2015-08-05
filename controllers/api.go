72cde1dcd4e9be4ca49afe87322f1da78a529203
tobe <tobeg3oogle@gmail.com>
2015-08-05 01:22:27 +0000

    Support get github user info

--- controllers/api.go
+++ controllers/api.go
@@ -14,8 +14,7 @@ import (
 
 	"golang.org/x/oauth2"
 
-	//"github.com/google/go-github/github"
-
+	"github.com/google/go-github/github"
 )
 
 // ApiController is the custom controller to provide APIs.
@@ -29,13 +28,14 @@ const (
 	redirectUrl        = ""
 )
 
+var oauthCfg *oauth2.Config
 
 func (c *ApiController) LoginGithub() {
 	log.Debug("Start to login github")
 
-	oauthCfg := &oauth2.Config{
-		ClientID:     "893d4b7aaab4c8c7bf52",
-		ClientSecret: "5df2d172df4e497f3eb66ef3a1ff3b6eb719a949",
+	oauthCfg = &oauth2.Config{
+		ClientID:     "69028d944609d7243cbf",
+		ClientSecret: "efe2f3d0a547f0840fd17411e729d20f02ece615",
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  githubAuthorizeUrl,
 			TokenURL: githubTokenUrl,
@@ -54,56 +54,43 @@ func (c *ApiController) LoginGithub() {
 	http.Redirect(c.Ctx.ResponseWriter, c.Ctx.Request, url, 302)
 }
 
-
 func (c *ApiController) LoginGithubCallback() {
 	fmt.Println("Start login github callback")
 
 	r := c.Ctx.Request
 
-	oauthCfg := &oauth2.Config{
-		ClientID:     "893d4b7aaab4c8c7bf52",
-		ClientSecret: "5df2d172df4e497f3eb66ef3a1ff3b6eb719a949",
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  githubAuthorizeUrl,
-			TokenURL: githubTokenUrl,
-		},
-		RedirectURL: redirectUrl,
-		Scopes:      []string{"repo"},
-	}
-
 	/*
-	if r.URL.Query().Get("state") != session.Values["state"] {
-		fmt.Println("no state match; possible csrf OR cookies not enabled")
-		return
-	}
+		if r.URL.Query().Get("state") != session.Values["state"] {
+			fmt.Println("no state match; possible csrf OR cookies not enabled")
+			return
+		}
 	*/
 
 	tkn, err := oauthCfg.Exchange(oauth2.NoContext, r.URL.Query().Get("code"))
 	if err != nil {
 		fmt.Println("there was an issue getting your token")
-		return
+		//return
 	}
+
+	fmt.Println(tkn)
 
 	if !tkn.Valid() {
 		fmt.Println("retreived invalid token")
-		return
+		//return
 	}
 
-	/*
 	client := github.NewClient(oauthCfg.Client(oauth2.NoContext, tkn))
-	*/
 
-	/*
 	user, _, err := client.Users.Get("")
 	if err != nil {
 		fmt.Println("error getting name")
-		return
+		//return
 	}
-	*/
+
+	fmt.Println(user)
 
 	url := "/account"
 
 	http.Redirect(c.Ctx.ResponseWriter, c.Ctx.Request, url, 302)
 
 }
-
